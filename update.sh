15d0086c5b9cdf93659ba108519c42294b34abe2
mediaberbagi <mediaberbagi.id@gmail.com>
2023-01-01 08:21:08 +0700

    remove some file

--- update.sh
+++
@@ -1,5 +0,0 @@
-git fetch --all --tags && git reset --hard origin/main
-export COMPOSER_HOME="$HOME/.config/composer";
-yes | composer install
-yes | php artisan migrate
-yes | php artisan db:seed --class=ConfigSeeder
--- update_dev.sh
+++
@@ -1,4 +0,0 @@
-git fetch --all --tags && git reset --hard origin/development
-yes | composer install
-yes | php artisan migrate
-yes | php artisan db:seed --class=ConfigSeeder
