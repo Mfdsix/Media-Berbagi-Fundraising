f813dbfc6846f605050af3c3b50cef986008b15a
Mahfudz Ainur Rif'an <mfdsix.1nd0@gmail.com>
2023-04-07 14:35:43 +0700

    enable config seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(SettingSeeder::class);
         $this->call(UsageSeeder::class);
-        // $this->call(ConfigSeeder::class);
-
+        $this->call(ConfigSeeder::class);
     }
 }
